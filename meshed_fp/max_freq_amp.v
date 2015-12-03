ea4af7e95205cd79f5bc5d38d8374b652aab0014
Gerzain Mata <grosas@mit.edu>
2015-12-03 08:39:29 -0500

    added testbench for maximum frequency and amplitude

--- meshed_fp/max_freq_amp.v
+++ meshed_fp/max_freq_amp.v
@@ -1,5 +1,5 @@
-module max_freq_amp (
-	input [10:0] hcount,
+module max_freq_amp #(parameter WIDTH = 1024)
+	( input [10:0] hcount,
 	input [9:0] amplitude,
 	output reg [12:0] max_freq,
 	output reg [9:0] max_amp
@@ -8,12 +8,14 @@ module max_freq_amp (
 	wire [11:0] freq;
 	assign freq = 3 * hcount;
 	
-	if(hcount == 0) begin
-		max_freq <= 4*hcount;
-		max_amp <= amplitude;
-	end else if(amplitude[9:0] > max_amp) begin
-		max_freq <= 4*hcount;
-		max_amp <= amplitude;
+	always @(*) begin
+		if(hcount == 0) begin
+			max_freq <= 4*hcount;
+			max_amp <= amplitude;
+		end else if(amplitude[9:0] > max_amp && hcount < WIDTH) begin
+			max_freq <= 4*hcount;
+			max_amp <= amplitude;
+		end
 	end
 
 endmodule
