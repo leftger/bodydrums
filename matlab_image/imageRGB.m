1146b086d56f3beb56a818f67bc8c31f1121f2da
Gerzain Mata <grosas@mit.edu>
2015-12-04 17:19:50 -0500

    .gitignore is now working

--- matlab_image/imageRGB.m
+++ matlab_image/imageRGB.m
@@ -10,7 +10,7 @@
 %% Getting 24 bit data
 %So when you look at a 24 bit bitmap file, the file specifies three 8 bit 
 %values for each color, 8 each for red, green, and blue.
-[picture] = imread('0.bmp');
+[picture] = imread('paulrevere6.bmp');
 
 %% View the image
 %This command image will draw the picture you just loaded
@@ -59,7 +59,7 @@ title('Red values in 24 bit bitmap')
 %table where each index specifies an r, g, and b value separately.
 %
 %Because of this, now we need to load both the image and it's colormap.
-[picture color_table] = imread('1.bmp');
+[picture color_table] = imread('paulrevere6.bmp');
 
 %% Displaying without the color table
 %If we try to display the picture without the colormap, the image does not
@@ -213,7 +213,7 @@ title('Regenerated Picture')
 data = dec2bin(pixel_columns,2);     %convert the binary data to 6 bit binary #s
 
 %open a file
-output_name = '1_image.coe';
+output_name = 'image_data.coe';
 file = fopen(output_name,'w');
 
 %write the header info
