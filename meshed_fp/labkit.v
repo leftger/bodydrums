2e019f633cd38ee82a8027ed53c31b4d90ba80d3
Gerzain Mata <grosas@mit.edu>
2015-12-05 12:45:32 -0500

    fixed missing wires in labkit.v (ac97_in,out, ready)

--- meshed_fp/labkit.v
+++ meshed_fp/labkit.v
@@ -62,77 +62,77 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 module labkit   (beep, audio_reset_b, ac97_sdata_out, ac97_sdata_in, ac97_synch,
-	       ac97_bit_clock,
-	       
-	       vga_out_red, vga_out_green, vga_out_blue, vga_out_sync_b,
-	       vga_out_blank_b, vga_out_pixel_clock, vga_out_hsync,
-	       vga_out_vsync,
+          ac97_bit_clock,
+          
+          vga_out_red, vga_out_green, vga_out_blue, vga_out_sync_b,
+          vga_out_blank_b, vga_out_pixel_clock, vga_out_hsync,
+          vga_out_vsync,
 
-	       tv_out_ycrcb, tv_out_reset_b, tv_out_clock, tv_out_i2c_clock,
-	       tv_out_i2c_data, tv_out_pal_ntsc, tv_out_hsync_b,
-	       tv_out_vsync_b, tv_out_blank_b, tv_out_subcar_reset,
+          tv_out_ycrcb, tv_out_reset_b, tv_out_clock, tv_out_i2c_clock,
+          tv_out_i2c_data, tv_out_pal_ntsc, tv_out_hsync_b,
+          tv_out_vsync_b, tv_out_blank_b, tv_out_subcar_reset,
 
-	       tv_in_ycrcb, tv_in_data_valid, tv_in_line_clock1,
-	       tv_in_line_clock2, tv_in_aef, tv_in_hff, tv_in_aff,
-	       tv_in_i2c_clock, tv_in_i2c_data, tv_in_fifo_read,
-	       tv_in_fifo_clock, tv_in_iso, tv_in_reset_b, tv_in_clock,
+          tv_in_ycrcb, tv_in_data_valid, tv_in_line_clock1,
+          tv_in_line_clock2, tv_in_aef, tv_in_hff, tv_in_aff,
+          tv_in_i2c_clock, tv_in_i2c_data, tv_in_fifo_read,
+          tv_in_fifo_clock, tv_in_iso, tv_in_reset_b, tv_in_clock,
 
-	       ram0_data, ram0_address, ram0_adv_ld, ram0_clk, ram0_cen_b,
-	       ram0_ce_b, ram0_oe_b, ram0_we_b, ram0_bwe_b, 
+          ram0_data, ram0_address, ram0_adv_ld, ram0_clk, ram0_cen_b,
+          ram0_ce_b, ram0_oe_b, ram0_we_b, ram0_bwe_b, 
 
-	       ram1_data, ram1_address, ram1_adv_ld, ram1_clk, ram1_cen_b,
-	       ram1_ce_b, ram1_oe_b, ram1_we_b, ram1_bwe_b,
+          ram1_data, ram1_address, ram1_adv_ld, ram1_clk, ram1_cen_b,
+          ram1_ce_b, ram1_oe_b, ram1_we_b, ram1_bwe_b,
 
-	       clock_feedback_out, clock_feedback_in,
+          clock_feedback_out, clock_feedback_in,
 
-	       flash_data, flash_address, flash_ce_b, flash_oe_b, flash_we_b,
-	       flash_reset_b, flash_sts, flash_byte_b,
+          flash_data, flash_address, flash_ce_b, flash_oe_b, flash_we_b,
+          flash_reset_b, flash_sts, flash_byte_b,
 
-	       rs232_txd, rs232_rxd, rs232_rts, rs232_cts,
+          rs232_txd, rs232_rxd, rs232_rts, rs232_cts,
 
-	       mouse_clock, mouse_data, keyboard_clock, keyboard_data,
+          mouse_clock, mouse_data, keyboard_clock, keyboard_data,
 
-	       clock_27mhz, clock1, clock2,
+          clock_27mhz, clock1, clock2,
 
-	       disp_blank, disp_data_out, disp_clock, disp_rs, disp_ce_b,
-	       disp_reset_b, disp_data_in,
+          disp_blank, disp_data_out, disp_clock, disp_rs, disp_ce_b,
+          disp_reset_b, disp_data_in,
 
-	       button0, button1, button2, button3, button_enter, button_right,
-	       button_left, button_down, button_up,
+          button0, button1, button2, button3, button_enter, button_right,
+          button_left, button_down, button_up,
 
-	       switch,
+          switch,
 
-	       led,
-	       
-	       user1, user2, user3, user4,
-	       
-	       daughtercard,
+          led,
+          
+          user1, user2, user3, user4,
+          
+          daughtercard,
 
-	       systemace_data, systemace_address, systemace_ce_b,
-	       systemace_we_b, systemace_oe_b, systemace_irq, systemace_mpbrdy,
-	       
-	       analyzer1_data, analyzer1_clock,
- 	       analyzer2_data, analyzer2_clock,
- 	       analyzer3_data, analyzer3_clock,
- 	       analyzer4_data, analyzer4_clock);
+          systemace_data, systemace_address, systemace_ce_b,
+          systemace_we_b, systemace_oe_b, systemace_irq, systemace_mpbrdy,
+          
+          analyzer1_data, analyzer1_clock,
+          analyzer2_data, analyzer2_clock,
+          analyzer3_data, analyzer3_clock,
+          analyzer4_data, analyzer4_clock);
 
    output beep, audio_reset_b, ac97_synch, ac97_sdata_out;
    input  ac97_bit_clock, ac97_sdata_in;
    
    output [7:0] vga_out_red, vga_out_green, vga_out_blue;
    output vga_out_sync_b, vga_out_blank_b, vga_out_pixel_clock,
-	  vga_out_hsync, vga_out_vsync;
+     vga_out_hsync, vga_out_vsync;
 
    output [9:0] tv_out_ycrcb;
    output tv_out_reset_b, tv_out_clock, tv_out_i2c_clock, tv_out_i2c_data,
-	  tv_out_pal_ntsc, tv_out_hsync_b, tv_out_vsync_b, tv_out_blank_b,
-	  tv_out_subcar_reset;
+     tv_out_pal_ntsc, tv_out_hsync_b, tv_out_vsync_b, tv_out_blank_b,
+     tv_out_subcar_reset;
    
    input  [19:0] tv_in_ycrcb;
    input  tv_in_data_valid, tv_in_line_clock1, tv_in_line_clock2, tv_in_aef,
-	  tv_in_hff, tv_in_aff;
+     tv_in_hff, tv_in_aff;
    output tv_in_i2c_clock, tv_in_fifo_read, tv_in_fifo_clock, tv_in_iso,
-	  tv_in_reset_b, tv_in_clock;
+     tv_in_reset_b, tv_in_clock;
    inout  tv_in_i2c_data;
         
    inout  [35:0] ram0_data;
@@ -165,7 +165,7 @@ module labkit   (beep, audio_reset_b, ac97_sdata_out, ac97_sdata_in, ac97_synch,
    output  disp_data_out;
    
    input  button0, button1, button2, button3, button_enter, button_right,
-	  button_left, button_down, button_up;
+     button_left, button_down, button_up;
    input  [7:0] switch;
    output [7:0] led;
 
@@ -179,7 +179,7 @@ module labkit   (beep, audio_reset_b, ac97_sdata_out, ac97_sdata_in, ac97_synch,
    input  systemace_irq, systemace_mpbrdy;
 
    output [15:0] analyzer1_data, analyzer2_data, analyzer3_data, 
-		 analyzer4_data;
+       analyzer4_data;
    output analyzer1_clock, analyzer2_clock, analyzer3_clock, analyzer4_clock;
 
    ////////////////////////////////////////////////////////////////////////////
@@ -312,6 +312,9 @@ module labkit   (beep, audio_reset_b, ac97_sdata_out, ac97_sdata_in, ac97_synch,
    SRL16 reset_sr (.D(1'b0), .CLK(clock_65mhz), .Q(power_on_reset),
 		   .A0(1'b1), .A1(1'b1), .A2(1'b1), .A3(1'b1));
    defparam reset_sr.INIT = 16'hFFFF;
+
+   wire [15:0] from_ac97_data, to_ac97_data;
+   wire ready;
 
    // ENTER button is user reset
    wire reset,user_reset;
